cf5cd24ce3dd0359377c5c2bf1bb9264c9477225
Brian Dunn and Jake Worth <dev+briandunn+jwworth@hashrocket.com>
2015-05-20 16:00:12 -0500

    Define views without drops

--- lib/dbcode.rb
+++ lib/dbcode.rb
@@ -1,9 +1,25 @@
 module DBCode
   extend self
   def ensure_freshness!
+    reset_search_path 'tmp'
     files = Dir[Rails.root.join('db/code/**/*.sql').expand_path]
-    files.each do |file|
-      ActiveRecord::Base.connection.execute File.read file
+    for file in files.sort
+      execute File.read file
     end
+  end
+
+  def reset_search_path(schema)
+    search_path = execute("show search_path").first.fetch 'search_path'
+    execute <<-SQL
+      drop schema if exists #{schema} cascade;
+      create schema if not exists #{schema};
+    SQL
+    unless search_path.split(',').first == schema
+      execute "set search_path to #{schema},#{search_path}"
+    end
+  end
+
+  def execute(sql)
+    ActiveRecord::Base.connection.execute sql
   end
 end
--- spec/features/dbcode_spec.rb
+++ spec/features/dbcode_spec.rb
@@ -8,14 +8,22 @@ module Rails
 end
 
 describe 'dbcode' do
-  before do
+  around do |test|
     ActiveRecord::Base.establish_connection(
       adapter: 'postgresql', database: 'dbcode_test'
     )
+    ActiveRecord::Base.connection.transaction do
+      test.call
+      raise ActiveRecord::Rollback
+    end
+  end
+
+  before do
+    Rails.root.join('db/code').rmtree
   end
 
   def create_view_file name, contents
-    Rails.root.join('db/code/views').tap(&:mkpath).join('foo.sql').write contents
+    Rails.root.join('db/code/views').tap(&:mkpath).join("#{name}.sql").write contents
   end
 
   def connection
@@ -23,19 +31,17 @@ describe 'dbcode' do
   end
 
   specify 'new connections use new declarations of a view' do
-
     create_view_file 'foo', <<-SQL
-      drop view if exists foo;
       create view foo as select 1 as number
     SQL
     DBCode.ensure_freshness!
 
     expect(connection.select_one('select number from foo')).to eq 'number' => '1'
     create_view_file 'foo', <<-SQL
-      drop view if exists foo;
       create view foo as select 2 as number
     SQL
     DBCode.ensure_freshness!
     expect(connection.select_one('select number from foo')).to eq 'number' => '2'
   end
+
 end
